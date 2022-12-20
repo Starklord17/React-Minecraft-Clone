926e4443d2ae5085ee1053a4631e408aec355e43
MaxSM <maxi.sm117@gmail.com>
2022-12-20 17:56:19 -0300

    :zap: keyboard

---
+++ src/hooks/useKeyboard.js
@@ -0,0 +1,43 @@
+import { useEffect } from "react"
+
+const ACTIONS_KEYBOARD_MAP = {
+  KeyW: 'moveForward',
+  KeyS: 'moveBackward',
+  KeyA: 'moveLeft',
+  KeyD: 'moveRight',
+  Space: 'jump',
+  Digit1: 'dirt',
+  Digit2: 'grass',
+  Digit3: 'glass',
+  Digit4: 'wood',
+  Digit5: 'log'
+}
+
+export const useKeyboard = () => {
+  const [actions, setActions] = useState({
+    moveForward: false,
+    moveBackward: false,
+    moveLeft: false,
+    moveRight: false,
+    jump: false,
+    dirt: false,
+    grass: false,
+    glass: false,
+    wood: false,
+    log: false
+  })
+
+  useEffect(() => {
+    const handleKeyDown = event => {
+      const { code } = event
+      console.log(code)
+    }
+    document.addEventListener('keydown', handleKeyDown)
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown)
+    }
+  }, [])
+}
+
+// min 54
