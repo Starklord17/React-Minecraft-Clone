bf9ec6cbba872515f08f4a22ae910bb476e049f5
MaxSM <maxi.sm117@gmail.com>
2022-12-20 13:52:09 -0300

    :sparkles: groundTexture

--- src/components/Ground.jsx
+++ src/components/Ground.jsx
@@ -1,15 +1,18 @@
 import { usePlane } from '@react-three/cannon'
+import { groundTexture } from '../images/textures'
 
 export function Ground () {
   const [ref] = usePlane(() => ({
-    rotation: [0, 0, 0],
-    position: [0, 0, 0]
+    rotation: [-Math.PI / 2, 0, 0], // x, y, z
+    position: [0, -0.5, 0] // x, y, z
   }))
+
+  groundTexture.repeat.set(100, 100)
 
   return (
     <mesh ref={ref}>
       <planeBufferGeometry attach='geometry' args={[100, 100]} />
-      <meshStandardMaterial attach='material' color='green' />
+      <meshStandardMaterial attach='material' map={groundTexture} />
     </mesh>
   )
 }
