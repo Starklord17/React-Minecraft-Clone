5c8c4b0ecf5403376c21e460f48022491addd117
MaxSM <maxi.sm117@gmail.com>
2022-12-20 13:39:24 -0300

    :zap: FPV = Free Point of View

---
+++ src/components/FPV.jsx
@@ -0,0 +1,14 @@
+import { PointerLockControls } from '@react-three/drei'
+import { useThree } from '@react-three/fiber'
+
+export function FPV () {
+  const { camera, gl } = useThree()
+
+  return (
+    <PointerLockControls
+      args={[camera, gl.domElement]}
+    />
+  )
+}
+
+// Free point of view
