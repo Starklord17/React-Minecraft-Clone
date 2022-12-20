e5a655d26fd00166d3ee034800dd50311a0fd85e
MaxSM <maxi.sm117@gmail.com>
2022-12-20 14:25:33 -0300

    :zap: Player move

---
+++ src/components/Player.jsx
@@ -0,0 +1,45 @@
+import { useSphere } from '@react-three/cannon'
+import { useFrame, useThree } from '@react-three/fiber'
+import { useEffect, useRef } from 'react'
+import { Vector3 } from 'three'
+
+export const Player = () => {
+  const { camera } = useThree()
+  const [ref, api] = useSphere(() => ({
+    mass: 1,
+    type: 'Dynamic',
+    position: [0, 5, 0] // Posición inicial
+  }))
+
+  const pos = useRef([0, 0, 0])
+  useEffect(() => {
+    api.position.subscribe(p => {
+      pos.current = p
+    })
+  }, [api.position])
+
+  // Velocidad del pj
+  const vel = useRef([0, 0, 0])
+  useEffect(() => {
+    api.velocity.subscribe(p => {
+      vel.current = p
+    })
+  }, [api.velocitiy])
+
+  // Cada vez que hace un Frame va hacia adelante. La posición de la camara copia la posición del pj.
+  useFrame(() => {
+    camera.position.copy(
+      new Vector3(
+        pos.current[0], // x
+        pos.current[1], // y
+        pos.current[2] // z
+      )
+    )
+
+    api.velocity.set(0, 0, -1)
+  })
+
+  return (
+    <mesh ref={ref} />
+  )
+}
